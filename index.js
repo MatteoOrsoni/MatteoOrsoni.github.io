3e6fb7796f0d2b5f395ddf63dd07ac2eb4fa9d38
MatteoOrsoni <129044339+MatteoOrsoni@users.noreply.github.com>
2024-07-10 17:18:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var max_length = 140;
+var string = prompt("Write Something");
+var stringCount = string.length;
+alert("You have written " + stringCount + " characters, you have " + (max_length - stringCount) + " characters left");
+
